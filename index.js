89ac4e6b81f63b7b98cd3201e686ba55a8b88c8f
Pauline Moraa <pmoraa431@gmail.com>
2025-03-06 03:43:41 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+console.log(multiply); 
+
+const random = Math.floor(Math.random() * 10) + 1; 
+
+const num3 = 44; 
+const num4 = 5;
+
+const mod = num3 % num4; 
+
+console.log(mod);
+
+const numbers = [10, 20, 15];
+const max = Math.max(...numbers);
+console.log(max); 
